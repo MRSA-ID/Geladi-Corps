3fe7c56dceb0e33352462876567860c1ac76bbe8
MRSA <sanggisda@gmail.com>
2026-08-19 05:30:59 +0700

    chore: update project configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,33 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"coolshapes-react": "coolshapes-react/dist/esm/coolshapes.js",
+			},
+		},
+		ssr: {
+			noExternal: ["coolshapes-react"],
+		},
 	},
 
 	output: "static",
-	integrations: [sitemap()],
+	image: {
+		remotePatterns: [
+			{
+				protocol: "https",
+			},
+		],
+		domains: ["deifkwefumgah.cloudfront.net", "images.unsplash.com"],
+	},
+	integrations: [react(), sitemap(), partytown(), robotsTxt()],
 });
